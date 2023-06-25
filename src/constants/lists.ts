a8cf5cd4e6c1704f1b31283913030a48476a7fc2
joshschcom <joshuaschiemann@yahoo.de>
2023-06-25 12:38:20 +0200

    1.0.5

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -4,39 +4,15 @@
  */
 export const UNSUPPORTED_LIST_URLS: string[] = []
 
-/*const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
-const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
-const AAVE_LIST = 'tokenlist.aave.eth'
-const SYNTHETIX_LIST = 'synths.snx.eth'
-const WRAPPED_LIST = 'wrapped.tokensoft.eth'
-const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
-const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
-const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
-const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
-const CMC_ALL_LIST = 'defi.cmc.eth'
-const CMC_STABLECOIN = 'stablecoin.cmc.eth'
-const KLEROS_LIST = 't2crtokens.eth'
-const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'*/
+/*
+https://raw.githubusercontent.com/conedex/default-token-list/main/build/conedex-default.tokenlist.json*/
 const CONEDEX_DEFAULT_LIST =
-  'https://raw.githubusercontent.com/conedex/default-token-list/main/build/conedex-default.tokenlist.json'
+  'https://raw.githubusercontent.com/conedex/community-token-list/main/build/conedex-community.tokenlist.json'
 const CONEDEX_COMMUNITY_LIST =
   'https://raw.githubusercontent.com/conedex/community-token-list/main/build/conedex-community.tokenlist.json'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  /*COMPOUND_LIST,
-  AAVE_LIST,
-  SYNTHETIX_LIST,
-  UMA_LIST,
-  WRAPPED_LIST,
-  SET_LIST,
-  OPYN_LIST,
-  ROLL_LIST,
-  COINGECKO_LIST,
-  CMC_ALL_LIST,
-  CMC_STABLECOIN,
-  KLEROS_LIST,
-  GEMINI_LIST,*/
   CONEDEX_DEFAULT_LIST,
   CONEDEX_COMMUNITY_LIST,
   ...UNSUPPORTED_LIST_URLS // need to load unsupported tokens as well
